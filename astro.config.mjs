1d1c737fa143bfa9f595ce24fdfd50c7bd1a3758
franciu89 <francesca7bonini@gmail.com>
2025-08-31 22:34:43 +0100

    Flat for rent (#5)
    * Astro init
    
    * Refactor Welcome component and update layout title; remove unused assets
    
    * Add dark mode styles to Welcome component
    
    * Add Welcome component styles and implement dark mode support
    
    * Replace favicon SVG with a new design featuring a rounded rectangle and stylized text
    
    * Implement carousel component and integrate flat rental details
    
    * Add site URL and trailing slash configuration to Astro config
    
    * Modified the Carousel into a React component
    
    * Using new Carousel component and updated page content
    
    * Outdoor pictures
    
    * Moved the pics into public; adjusted styles when adding them to the page
    
    * Added floorplan
    
    * Floorplan displays in the gallery, added disclaimer on its accuracy
    
    * Pictures of the flat
    
    * Added pictures to gallery
    
    * Fixing aspect ratio of pictures so they show better in the gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({ server: { host: "127.0.0.1" } });
+export default defineConfig({
+  server: { host: "127.0.0.1" },
+  site: "https://franciu89.github.io",
+  trailingSlash: "ignore",
+  integrations: [react()],
+});
